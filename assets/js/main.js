4a58e4a8cf27f73323a08e6cf71772f3b0e867f0
felipecruz159 <felipe.cruz159@hotmail.com>
2023-04-19 19:07:06 -0300

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,25 @@
+
+function convertPokemonToLi(pokemon) {
+    return `<li class="pokemon">
+    <span class="number">#001</span>
+    <span class="name">${pokemon.name}</span>
+
+    <div class="detail">
+        <ol class="types">
+            <li class="type">grass</li>
+            <li class="type">poison</li>
+        </ol>
+
+        <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/1.svg"
+            alt="${pokemon.name}">
+    </div>
+</li>`;
+}
+
+const pokemonList = document.getElementById('pokemonList');
+
+pokeApi.getPokemons().then((pokemons = []) => {
+    const newHtml = pokemons.map(convertPokemonToLi).join('')
+    pokemonList.innerHTML += newHtml
+})
+    
