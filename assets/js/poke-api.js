2cd2da807acce14d1b119b6db2a68e46460c03fe
felipecruz159 <felipe.cruz159@hotmail.com>
2023-04-19 20:02:31 -0300

    dynamic layout

--- assets/js/poke-api.js
+++ assets/js/poke-api.js
@@ -1,9 +1,17 @@
 const pokeApi = {}
 
+pokeApi.getPokemonDetail = (pokemon) => {
+    return fetch(pokemon.url).then((response) => response.json()) // converte a response para json
+}
+
 pokeApi.getPokemons = (offset = 0, limit = 10) => {
     const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`;
+
     return  fetch(url)
-        .then((response) => response.json())
-        .then((jsonBody) => jsonBody.results)
-        .catch((error) => console.log(error))
+        .then((response) => response.json()) // pega a resposta e transforma em json
+        .then((jsonBody) => jsonBody.results) // especifica que quer os resultados
+        .then((pokemons) => pokemons.map(pokeApi.getPokemonDetail)) // detalhes do pokemon
+        .then((detailRequests) => Promise.all(detailRequests)) //esperando que todas as requisicoes terminem
+        .then((pokemonDetails) => pokemonDetails) //retorna os pokemons com detalhes
 }
+
